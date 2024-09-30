43f27a9361feea735dd16191ed36221e087fe051
Taisiia <taya13taya@gmail.com>
2024-09-30 20:58:46 +0300

    adaptiv added

--- src/footer_components/Footer.tsx
+++ src/footer_components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   console.log('footerLinks:', footerLinks); 
     return (
         <footer className="footer">
-        <div className="footer-content" style={{ display: 'flex', justifyContent: 'space-around' }}>
+        <div className="footer-content"  style={{ display: 'flex', justifyContent: 'space-around' }}>
           {footerLinks.map((category, index) => (
             <FooterMenuComponent  key={`${category.title}-${index}`} title={category.title} links={category.links} />
           ))}
